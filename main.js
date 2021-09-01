2b45ae694e714f82370965285b92f493107f8195
SBSstudio <86954238+SBSstudio@users.noreply.github.com>
2021-09-01 05:14:21 -0700

    Update main.js

--- main.js
+++ main.js
@@ -40,7 +40,7 @@ bot.on('message', async(ctx) => {
         var weather_data = (await axios.get(`${config.API_URL}${ctx.message.text}`)).data
         if (weather_data.error === null && weather_data.result.length !== 0) {
             var data = weather_data.result[0]
-            ctx.replyWithHTML(`Today's weather in <b><u>${data.location.name}</u></b>\n\n<b>${data.current.skytext}</b>\n\n📆 Date : <b>${data.current.date}</b>\n🌫 Sky code : <b>${data.current.skycode}</b>\n🌡 Temperature : <b>${data.current.temperature} °C</b>\n⛅️Feels like : <b>${data.current.feelslike} °C</b>\n💧 Humidity : <b>${data.current.humidity} %</b>\n💨 Wind speed : <b>${data.current.winddisplay}</b>\n\n⏰Last Update : <b>${data.current.observationtime}</b><em>\n\n@TGweatherrobot|@SBS_Studio</em>`);
+            ctx.replyWithHTML(`Today's weather in <b><u>${data.location.name}</u></b>\n\n<b>${data.current.skytext}</b>\n\n📆 Date : <b>${data.current.date}</b>\n🌫 Sky code : <b>${data.current.skycode}</b>\n🌡 Temperature : <b>${data.current.temperature} °C</b>\n⛅️ Feels like : <b>${data.current.feelslike} °C</b>\n💧 Humidity : <b>${data.current.humidity} %</b>\n💨 Wind speed : <b>${data.current.winddisplay}</b>\n\n⏰Last Update : <b>${data.current.observationtime}</b><em>\n\n@TGweatherrobot|@SBS_Studio</em>`);
         } else {
             ctx.reply("Error ❌\n\nKindly send your city/village name again.\nMake sure if there is a space in your city/village name give that space or try with a big city name nearby you.\n\n@TGweatherrobot|@SBS_Studio");
         }
