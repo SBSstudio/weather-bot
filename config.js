0af753b56e06d14343cb26a9ce66e959c9452664
SBSstudio <86954238+SBSstudio@users.noreply.github.com>
2021-09-01 04:12:47 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    BOT_TOKEN: '1352267645:AAEjxI-sAlt82TFxgj-k6dOk7MqBP2Jrj6Q',
+    BOT_TOKEN: '1814297026:AAE31jjXUUUWE9ZJ5QgL-Q0ZfSsncOsE_O8',
     API_URL: 'https://num1mt.deta.dev/location/'
 }
